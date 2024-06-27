9b70be2d038fdc76cddcdc89175b9a33db583241
KIM SOOBIN <xpsxm225@naver.com>
2024-06-27 22:44:34 +0900

    Feat: GooleLoginBtn Component 정의

---
+++ src/components/GoogleLoginButton.jsx
@@ -0,0 +1,24 @@
+
+
+
+import { GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+export const GoogleLoginButton = () => {
+    const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+    
+    return (
+        <>
+            <GoogleOAuthProvider clientId={clientId}>
+                <GoogleLogin
+                    onSuccess={credentialResponse => {
+                        console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                />
+            </GoogleOAuthProvider>
+        </>
+    )
+}
