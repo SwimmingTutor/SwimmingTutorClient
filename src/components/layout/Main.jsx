2f8ecb81561cd8c6f64b3c101656b0af5e5c5702
KIM SOOBIN <xpsxm225@naver.com>
2024-07-01 15:35:07 +0900

    Refactor: MainLayout 파일명 Main으로 변경, 컴포넌트 수정

--- src/components/layout/Main.jsx
+++
@@ -1,8 +0,0 @@
-export const Main = ({children}) => {
-    let mainClassName = "relative w-full h-fit min-h-screen p-2 bg-slate-50/65"
-    return (
-        <main id="main" className={mainClassName}>
-            {children}
-        </main>
-    )
-}
