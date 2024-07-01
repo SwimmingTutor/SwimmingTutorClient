9e5c52a07eca26760c03cb920065f7249647d78b
KIM SOOBIN <xpsxm225@naver.com>
2024-07-01 15:15:07 +0900

    Refactor: Nav Component 수정

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,5 @@
+import { Link, useLocation } from 'react-router-dom';
+
 import HomeIcon from '../assets/icons/home.svg';
 import HomeIconActiveIcon from '../assets/icons/home-active.svg';
 
@@ -14,63 +16,68 @@ import MyActiveIcon from '../assets/icons/my-active.svg';
 const NAV_ELEMENT_INFO = [
     {
         name: "홈",
-        link: "",
+        page: "",
         icon: HomeIcon,
         active_icon: HomeIconActiveIcon,
     },
     {
         name: "수영장",
-        link: "",
+        page: "swimmingpool",
         icon: SwimmingPoolIcon,
         active_icon: SwimmingPoolActiveIcon,
     },
     {
         name: "루틴",
-        link: "",
+        page: "routine",
         icon: RoutineIcon,
         active_icon: RoutineActiveIcon,
     },
     {
         name: "마이",
-        link: "",
+        page: "my",
         icon: MyIcon,
         active_icon: MyActiveIcon,
     },
 ];
 
-const NavElement = ({ link, name, icon, activeIcon, isActive }) => {
+const NavElement = ({ page, name, icon, activeIcon }) => {
+    const location = useLocation();
+    let path = location.pathname.split('/')[1];
+    // let path = location.pathname.split('/').pop();
+    const isActive=path===page;
+
     let iconSrc = icon;
-    let textColor = " text-neutral-500";
+    let textColor = " ";
     if (isActive) { 
         iconSrc = activeIcon; 
         textColor = " text-primary";
     }
 
-    function handleClickNavButton() {
-        // link props use
-    }
     return (
-        <button className='cursor-pointer w-9' onClick={handleClickNavButton}>
-            <img src={iconSrc} alt={name} />
-            <p className={"text-xs"+textColor}>{name}</p>
-        </button>
+        <Link className='cursor-pointer w-9 flex flex-col justify-between items-center' to={"/"+page}>
+            <img src={iconSrc} alt={name} className='w-4/6'/>
+            {/* <p className={"w-full text-xs text-center tracking-tighter "+textColor}>{name}</p> */}
+        </Link>
     )
 }
 
 export const Nav = () => {
     return (
-        <aside id="dock" className="fixed bottom-0 w-full h-28 z-50 bg-white">
-            <div className="dock-container w-[440px] m-auto">
-                <nav className="dock-menu w-full px-10 py-5 flex flex-row justify-around gap-16 border-t-[0.1px] border-b-neutral-200">
+        <aside id="dock" className="fixed bottom-0 w-full h-14 z-40">
+            <div className="dock-container container w-app h-full">
+                <nav className="
+                                dock-menu w-full h-full px-4 pt-4 pb-3 
+                                flex flex-row justify-around gap-16 
+                                border-t-[0.1px] border-b-neutral-200 rounded-t-lg bg-white
+                                ">
                     {
                         NAV_ELEMENT_INFO.map((item) => (
                             <NavElement
                                 key={item.name}
-                                link={item.link}
+                                page={item.page}
                                 name={item.name}
                                 icon={item.icon}
                                 activeIcon={item.active_icon}
-                                isActive={false}
                             />
                         )
                         )
