1ff0f93a3108a511fb906ec6694c257d75d29b9b
KIM SOOBIN <xpsxm225@naver.com>
2024-07-01 15:16:13 +0900

    Chore: tailwind primary 세부 정의, app 사이즈 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,26 @@ module.exports = {
       center: true,
     },
     extend: {
-      colors: {
-        "primary": "#0B0084", // main color
+      width: {
+        'app': '27.5rem',
       },
-    }
+      colors: {
+        'primary': {
+          '50': '#f1f3ff',
+          '100': '#e5e6ff',
+          '200': '#ced1ff',
+          '300': '#a7aaff',
+          '400': '#7876ff',
+          '500': '#4c3fff',
+          '600': '#3518ff',
+          '700': '#2407fa',
+          '800': '#1e05d2',
+          '900': '#1a06ac',
+          '950': '#0b0084',
+          DEFAULT: "#0b0084",
+        },
+      },
+    },
   },
   plugins: []
 };
