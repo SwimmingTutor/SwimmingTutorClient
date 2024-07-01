162e60fc7c28535f4c56fa08ab224a6228ffad8f
KIM SOOBIN <xpsxm225@naver.com>
2024-07-01 15:44:44 +0900

    Feat: 404 Page 정의

---
+++ src/components/PageInfoText.jsx
@@ -0,0 +1,8 @@
+export const PageInfoText = ({ title, content }) => {
+    return (
+        <div className="flex flex-col gap-5 cursor-default text-center">
+            <div className='font-extrabold text-primary text-5xl'>{title}</div>
+            <div>{content}</div>
+        </div>
+    )
+}
