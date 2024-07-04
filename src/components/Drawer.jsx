14b3a463cc5ed13b5154e8e997ff7eac10ea9366
KIM SOOBIN <xpsxm225@naver.com>
2024-07-04 16:19:57 +0900

    Refactor: - Drawer Component createPortal로 DOM 트리 구조 변경.
    - ref객체로 drawer 조작하는 커스텀 함수 정의.

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import { createPortal } from 'react-dom';
 
-// const toggleDrawer = () => {
-//     setIsDrawerOpen((prevState) => !prevState);
-//   };
+export const Drawer = forwardRef(({ children, title }, ref) => {
+    let [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const toggleDrawer = () => {
+        setIsDrawerOpen((prevState) => !prevState);
+      };
 
-export const Drawer = ({ children, isDrawerOpen, toggleDrawer, title }) => {
-    return (
-        <div className="relative">
+    useImperativeHandle(ref, ()=>{
+        return {
+            draw(){
+                toggleDrawer();
+            }
+        }
+    })
+    return createPortal(
+        (<div className="relative">
             <div
                 className={`fixed bottom-0 left-0 w-full h-fit min-h-60 max-h-60 bg-transparent shadow-lg z-[60] transition-transform duration-400 ${isDrawerOpen ? 'transform translate-y-0' : 'transform translate-y-full'
                     }`}
@@ -14,11 +23,11 @@ export const Drawer = ({ children, isDrawerOpen, toggleDrawer, title }) => {
                 <div className="container relative w-app h-full 
                                 flex flex-col justify-start
                                 rounded-t-lg shadow-lg">
-                        <div className='sticky top-0 
+                    <div className='sticky top-0 
                                         w-app h-full px-4 py-3 bg-white rounded-t-lg
                                         text-center text-base font-semibold text-primary'>{title}</div>
-                        {/* Drawer content */}
-                        {children}
+                    {/* Drawer content */}
+                    {children}
 
                 </div>
             </div>
@@ -29,6 +38,6 @@ export const Drawer = ({ children, isDrawerOpen, toggleDrawer, title }) => {
                     onClick={toggleDrawer}
                 />
             )}
-        </div>
+        </div>), document.getElementById('drawer-root')
     );
-};
+});
