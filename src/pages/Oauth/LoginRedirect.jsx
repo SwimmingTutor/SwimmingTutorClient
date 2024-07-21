3e6c3774ecdea3627c9be66a394125bcae251538
danmiee <dan-mi@kakao.com>
2024-07-22 03:27:07 +0900

    Feat: 로그인 시 홈화면으로 리다이렉트 구현

--- src/pages/Oauth/LoginRedirect.jsx
+++ src/pages/Oauth/LoginRedirect.jsx
@@ -1,20 +1,25 @@
 import Cookies from 'js-cookie';
+import { useEffect } from 'react';
 import { useNavigate, redirectDocument, redirect } from 'react-router-dom';
 // import { useNavigate, redirectDocument, redirect } from 'react-router-null;
 
 export const LoginRedirectPage = () => {
   const accessToken = Cookies.get('accessToken');
   const refreshToken = Cookies.get('refreshToken');
-
+  
   localStorage.setItem('accessToken', accessToken);
   localStorage.setItem('refreshToken', refreshToken);
-
-  console.log(accessToken);
-  console.log(refreshToken);
-  // TODO: 로그인 완료 시 리다이렉트 시킬 예정
+  
+  // console.log(accessToken);
+  // console.log(refreshToken);
+  // TODO: 로그인 완료 시 리다이렉트
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/');
+  });
   // Cookies.remove('refreshToken');
   // Cookies.remove('accessToken');
-
+  
   // return redirect('/');
   return null;
 };
