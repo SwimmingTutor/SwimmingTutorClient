6baf1a98ecd4b21ea7c8e331f2ed272fad92179a
danmiee <dan-mi@kakao.com>
2024-07-28 20:14:44 +0900

    Feat: 내 수영 레벨 조회 axios  적용

--- src/components/My/LevelList.jsx
+++ src/components/My/LevelList.jsx
@@ -1,40 +1,51 @@
-import React from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import Menu from '../Menu.jsx';
-
-const levels = [
-  {
-    key: 'freestyle',
-    name: '자유형',
-    level: '-'
-  },
-  {
-    key: 'backstroke',
-    name: '배영',
-    level: '-'
-  },
-  {
-    key: 'breathstroke',
-    name: '평영',
-    level: '-'
-  },
-  {
-    key: 'butterfly',
-    name: '접영',
-    level: '-'
-  }
-];
-
-const LEVEL_ELEMENT_INFO = levels.map(item => ({
-  key: item.key,
-  name: `${item.name} / ${item.level}`,
-  path: `/my/level/test/${item.key}`
-}));
+import axios from '../../utils/https/axios/customAxios';
+import INITIALLEVELS from '../../constants/initialLevels.js';
 
 const LevelList = () => {
+  const [levels, setLevels] = useState(INITIALLEVELS);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await axios.get('/level', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+      });
+      const data = response.data;
+
+      setLevels(prevLevels =>
+        prevLevels.map(level => ({
+          ...level,
+          level: data[level.key] || '-'
+        }))
+      );
+    } catch (err) {
+      setError('Failed to fetch levels. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <div className='flex h-fit w-full flex-col gap-3 text-[0.9rem] font-light'>
-      {LEVEL_ELEMENT_INFO.map(item => (
-        <Menu key={item.key} name={item.name} path={item.path} />
+      {levels.map(item => (
+        <Menu key={item.key} name={`${item.name} / ${item.level}`} path={`/my/level/test/${item.key}`} />
       ))}
     </div>
   );
