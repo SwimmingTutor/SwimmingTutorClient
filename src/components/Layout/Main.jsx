b49b1c577a0352858e107e9ce86544724cea40dd
ekmbjh <ekmbjh@naver.com>
2024-08-03 14:37:14 +0900

    Fix : Header 고정 오류 조치

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,6 +1,6 @@
 const Main = ({ children }) => {
   return (
-    <main id='main' className='relative h-fit min-h-max w-full bg-slate-50/65 px-5 pb-3 pt-8'>
+    <main id='main' className='relative h-fit min-h-max w-full bg-slate-50/65 px-5 pb-3 pt-20'>
       {children}
     </main>
   );
