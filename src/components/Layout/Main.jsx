9d1ffe8090d15d35a050185016691bd2a5243018
KIM SOOBIN <xpsxm225@naver.com>
2024-08-06 11:17:00 +0900

    Fix: main content scroll 동작 정상 동작하하도록 수정

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,7 +1,9 @@
 const Main = ({ children }) => {
   return (
-    <main id='main' className='relative h-fit min-h-max w-full bg-slate-50/65 px-5 pb-3 pt-20'>
-      {children}
+    <main id='main' className='relative w-full bg-slate-50/65 px-5 pb-3 pt-20 overflow-y-auto flex-1'>
+      <div className='min-h-full pb-14'>
+        {children}
+      </div>
     </main>
   );
 };
